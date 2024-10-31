beaaf8ab298e8f120ba6cb29712dd1afec293fa9
Joy H <joyhwangbvt2023@gmail.com>
2024-10-31 00:24:46 -0700

    Altered the passing of props via object to make Card component slightly more readable, as well as added some more code commentary and notes

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ import Card from "./components/Card"
 import data from "./data"
 
 function App() {
-  const uniqueCards = data.map((info) => {
+  const uniqueCards = data.map((info) => { // Mapping the data from data.jsx onto a new card component for each data object
     return (
       < Card
-        key={info.id}
-        img={info.coverImg}
-        rating={info.stats.rating}
-        reviewCount={info.stats.reviewCount}
-        location={info.location}
-        title={info.title}
-        price={info.price}
-        openSpots={info.openSpots}// Creating prop called openSpots
+        key={info.id} // The key is needed when using .map() method
+        {...info} // This spread operator usage for spreading props 
+
+      // The mass of shorthand notation below can be reduced to singular info={info} object as well, with the caveat that every props accessed in the child component must follow a "props.info.___"" format
+      /*
+      img={info.coverImg}
+      rating={info.stats.rating}
+      reviewCount={info.stats.reviewCount}
+      location={info.location}
+      title={info.title}
+      price={info.price}
+      openSpots={info.openSpots}// Creating prop called openSpots
+      */
       />
     )
   });
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
 
-function Card(props) {
-    console.log(props.openSpots)
+function Card(props) { // Passing in props from App.jsx
+    let badgeText; // Creating flexibility: depending on value of badgeText, the card badge will display different text or not display at all 
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT";
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE";
+    } 
+
     return (
         <div className="card">
-        {props.openSpots===0 && <div className="card-badge bold-text">SOLD OUT</div>}
-            <img className="card-img" src={new URL(`../assets/images/${props.img}`, import.meta.url)} />
+        {badgeText && <div className="card-badge bold-text">{badgeText}</div>}
+        {/* Recalling the notation for conditionals in JS */}
+            <img className="card-img" src={new URL(`../assets/images/${props.coverImg}`, import.meta.url)} />
+            {/* Importing images via import.meta.url due to folder nesting in assets and images folders as an alternative to utilizing the public folder to store all images */}
             <div className="reviews">
                 <img className="rating-img" src={new URL("./../assets/images/star.png", import.meta.url)} />
                 <span>{props.rating} </span>
@@ -18,8 +26,9 @@ function Card(props) {
     )
 }
 
-Card.propTypes = {
-    img: PropTypes.string,
+Card.propTypes = { // Setting the PropTypes to ensure the correct type of data being passed into our component
+    // info: PropTypes.object, // This is utilized only if passed in props was packed into an object to be passed down, such as in info={info}
+    coverImg: PropTypes.string,
     rating: PropTypes.number,
     reviewCount: PropTypes.number,
     location: PropTypes.string,
@@ -30,16 +39,21 @@ Card.propTypes = {
 
 export default Card;
 
-// <img src= { new URL("../images/asdf.png", import.meta.url)} className="randomImage"/>
+/* Other notes below on Image relative path issue fixing */
 
-// try npm run build to make a distribution dist folder
+// Example --> <img src= { new URL("../images/asdf.png", import.meta.url)} className="randomImage"/>
+
+// Try npm run build to make a distribution dist folder if you happen need it for other troubleshooting. We don't really want to commit these to github, however.
 
 /* Another note: If I have trouble with vite incorrectly reading my images from the relative path not pointing to the correct parent file:
 
 Go into vite.config.js file:
 go into the export define config function
 
-base: "./", // Whenever we open a project it is gonna base it NOT on the root directory of url, but one level up above the file we work on (to address intense levels of nesting) in current directory rather than random file in assets or src
-*/
+Potential workaround: --> Adding base: "./", into vite.config.js file 
+// Whenever we open a project it is going to by default base it on the root directory of url, but one level up above the file we work on (to address intense levels of nesting) in current directory rather than random file in assets or src
+// --> However, that won't be needed in my case, I've got multiple folders being nested so I will not be changing the vite.config.js file necessarily to cater for "./", and it seems that leaving the configurations as is will do.
 
-// After fixing things up in base, we run npm run build again!
+// Should we aim to fix things up in base, we would theoretically run npm run build again to update the distributable folder.
+
+*/
